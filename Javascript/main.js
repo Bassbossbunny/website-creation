68ad65d5f76e08ac4e8ff0f798da7930c074b93c
Rachel.P <rachel.payette@edu.sait.ca>
2024-11-17 14:09:52 -0700

    js: Time-dependent greetings
    added morning, afternoon, night. Fun sentences, chose some colors in CSS that aren't terrible. Hard to have contrast with each other if using set site pallet of violet tones, so went with more traditional color options to represent times of day. Will change later to more pallet-constrained options after assignment is handed in.

--- Javascript/main.js
+++ Javascript/main.js
@@ -10,4 +10,22 @@ window.onload = function() {
     console.log("Current Date: " + fullDate);
 };
 
+// Hello User - based on time of day
 
+window.onload = function() {
+    const currentHour = new Date().getHours(); // reminder!! it is a 0-23. JS is zero indexed!!!
+    const HelloMessage = document.getElementById("Hello-Message");
+    const HelloContainer = document.getElementById("Hello-User");
+
+    // gosh darn loops, I tell ya... reminder check notes for shorthand symbols. 
+    if (currentHour < 12) {
+        HelloMessage.textContent = "Well, don't you look lovely this morning!";
+        greetingContainer.classList.add("morning");
+    } else if (currentHour >= 12 && currentHour < 17) {
+        HelloMessage.textContent = "Such a lovely afternoon to look at some art!";
+        HelloContainer.classList.add("afternoon");
+    } else {
+        HelloMessage.textContent = "Up late, are we? Hope you have a lovely evening!";
+        HelloContainer.classList.add("evening");
+    }
+};
